38a653afa9d863b69c06f558ef54b24afc73e4eb
GroweredTKTK <thomas.saija@tktk.ee>
2023-09-06 00:21:15 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter your name: "); //Output
+
+string userName = Console.ReadLine(); //Input
+
+if (userName == "Thomas")
+    Console.WriteLine($"Hi, Admin {userName} <3"); //Output for the code developer
+else
+    Console.WriteLine($"Hello, {userName}!"); //Output for regular user
